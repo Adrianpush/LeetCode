c3e7708cbe45ba9ee79c983aaf0efc82125f8892
adrian <adrian.puscu@gmail.com>
2023-08-25 02:31:13 +0300

    Solved 2011.

---
+++ src/java_solutions/FinalValueOfVariableAfterPerformingOperations.java
@@ -0,0 +1,29 @@
+package src.java_solutions;
+
+public class FinalValueOfVariableAfterPerformingOperations {
+
+    /**
+     * 2011. Final Value of Variable After Performing Operations
+     * There is a programming language with only four operations and one variable X:
+     * <p>
+     * ++X and X++ increments the value of the variable X by 1.
+     * --X and X-- decrements the value of the variable X by 1.
+     * <p>
+     * Initially, the value of X is 0.     *
+     * Given an array of strings operations containing a list of operations,
+     * return the final value of X after performing all the operations.
+     */
+    class Solution {
+        public int finalValueAfterOperations(String[] operations) {
+            int answerValue = 0;
+            for (String operation : operations) {
+                if (operation.contains("-")){
+                    answerValue -=1;
+                } else {
+                    answerValue += 1;
+                }
+            }
+            return answerValue;
+        }
+    }
+}
